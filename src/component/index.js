5fb24736a69034d45bf9029810b790606b011786
Adam Hepton <adam@hepton.org>
2020-07-20 18:10:37 +0100

    Removed Default Styles

--- src/component/index.js
+++ src/component/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './index.css';
+// import './index.css';
 import FlipMove from 'react-flip-move';
 import UploadIcon from './UploadIcon.svg';
 
